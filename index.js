5537c27b385f18618aeaf29b43e16867f570fff2
Tuan Anh Tran <me@tuananh.org>
2017-12-01 15:53:44 +0700

    fix: check empty input

--- index.js
+++ index.js
@@ -19,17 +19,23 @@ fs.exists(path, exists => {
     if (exists) {
         console.log('.gitignore already exists')
     } else {
-        const argv = cli.input.map(s => s.toLowerCase()).join('%2C')
-        const url = 'https://www.gitignore.io/api/' + argv
-        https
-            .get(url, res => {
-                res.pipe(fs.createWriteStream(path, 'utf-8'))
-            })
-            .on('close', () => {
-                console.log('Added .gitignore in current directory')
-            })
-            .on('error', e => {
-                console.log('Unable to generate .gitignore file')
-            })
+        if (!cli.input.length) {
+            console.log(
+                'you need to provide some keyword(s). eg: $ deny macos node'
+            )
+        } else {
+            const argv = cli.input.map(s => s.toLowerCase()).join('%2C')
+            const url = 'https://www.gitignore.io/api/' + argv
+            https
+                .get(url, res => {
+                    res.pipe(fs.createWriteStream(path, 'utf-8'))
+                })
+                .on('close', () => {
+                    console.log('Added .gitignore in current directory')
+                })
+                .on('error', e => {
+                    console.log('Unable to generate .gitignore file')
+                })
+        }
     }
 })
